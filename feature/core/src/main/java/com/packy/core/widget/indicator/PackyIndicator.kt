950d56de04030954c3d9331b2d75aea40128c1bb
KimHyungSik <colrog968@gmail.com>
2024-01-09 01:38:04 +0900

    PackyIndicator

---
+++ feature/core/src/main/java/com/packy/core/widget/indicator/PackyIndicator.kt
@@ -0,0 +1,44 @@
+package com.packy.core.widget.indicator
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.packy.core.theme.PackyTheme
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PackyIndicator(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState,
+) {
+    Row(
+        modifier = modifier
+            .wrapContentHeight()
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        repeat(pagerState.pageCount) { iteration ->
+            val color =
+                if (pagerState.currentPage == iteration) PackyTheme.color.gray900 else PackyTheme.color.gray300
+            Box(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .clip(CircleShape)
+                    .background(color)
+                    .size(8.dp)
+            )
+        }
+    }
+}

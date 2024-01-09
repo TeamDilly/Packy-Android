bf5effdcfc6ef70470783cf5cf2becd31cb8d723
KimHyungSik <colrog968@gmail.com>
2024-01-09 23:52:22 +0900

    PaginationAnimation

--- feature/core/src/main/java/com/packy/core/animations/PaginationAnimation.kt
+++ feature/core/src/main/java/com/packy/core/animations/PaginationAnimation.kt
@@ -1,2 +1,50 @@
 package com.packy.core.animations
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
+import java.time.Duration
+
+object PaginationAnimation {
+    fun slidInTop(
+        durationMillis: Int = 300
+    ): EnterTransition {
+        return slideInVertically(
+            initialOffsetY = { fullHeight -> fullHeight },
+            animationSpec = tween(durationMillis = durationMillis, easing = LinearOutSlowInEasing)
+        )
+    }
+
+    fun slidOutToBottom(
+        durationMillis: Int = 300
+    ): ExitTransition {
+        return slideOutVertically(
+            targetOffsetY = { fullHeight -> -fullHeight },
+            animationSpec = tween(durationMillis = durationMillis, easing = LinearOutSlowInEasing)
+        )
+    }
+
+    fun slidInToStart(
+        durationMillis: Int = 300
+    ): EnterTransition {
+        return slideInHorizontally(
+            initialOffsetX = { fullWidth -> fullWidth },
+            animationSpec = tween(durationMillis = durationMillis, easing = LinearOutSlowInEasing)
+        )
+    }
+
+    fun slidOutToEnd(
+        durationMillis: Int = 300
+    ): ExitTransition {
+        return slideOutHorizontally(
+            targetOffsetX = { fullWidth -> -fullWidth },
+            animationSpec = tween(durationMillis = durationMillis, easing = LinearOutSlowInEasing)
+        )
+    }
+}
--- feature/onboarding/src/main/java/com/packy/onboarding/navigation/OnboardingNavigation.kt
+++ feature/onboarding/src/main/java/com/packy/onboarding/navigation/OnboardingNavigation.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.packy.core.animations.PaginationAnimation
 import com.packy.onboarding.login.LoginScreen
 import com.packy.onboarding.onboarding.OnboardingScreen
 import com.packy.onboarding.signupnickname.SignupNickNameScreen
@@ -17,16 +18,31 @@ fun NavGraphBuilder.onboardingNavGraph(
     ) {
         composable(
             route = OnboardingRoute.ONBOARDING,
+            enterTransition = {
+                PaginationAnimation.slidInTop()
+            }, exitTransition = {
+                PaginationAnimation.slidOutToBottom()
+            }
         ) {
             OnboardingScreen(navController = navController)
         }
         composable(
             route = OnboardingRoute.LOGIN,
+            enterTransition = {
+                PaginationAnimation.slidInToStart()
+            }, exitTransition = {
+                PaginationAnimation.slidOutToEnd()
+            }
         ) {
             LoginScreen(navController = navController)
         }
         composable(
-            route = OnboardingRoute.SIGNUP_NICKNAME
+            route = OnboardingRoute.SIGNUP_NICKNAME,
+            enterTransition = {
+                PaginationAnimation.slidInToStart()
+            }, exitTransition = {
+                PaginationAnimation.slidOutToEnd()
+            }
         ) {
             SignupNickNameScreen(navController = navController)
         }

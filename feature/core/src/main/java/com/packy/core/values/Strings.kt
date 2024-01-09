0f911a1bfa412266b9579a4cc08733908ac8a34b
KimHyungSik <colrog968@gmail.com>
2024-01-10 00:05:55 +0900

    Strings

--- feature/core/src/main/java/com/packy/core/values/Strings.kt
+++ feature/core/src/main/java/com/packy/core/values/Strings.kt
@@ -1,9 +1,19 @@
 package com.packy.core.values
 
 object Strings {
+    // common
     const val START = "시작하기"
     const val NEXT = "다음"
-    const val ONBOARDING_TITLE = "패키와 함께 특별한 선물박스를 만들어보세요."
-    const val LOGIN_TITLE = "마음으로 채우는 특별한 선물박스"
     const val SKIP = "건너뛰기"
+    const val SAVE = "저장"
+
+    // onboarding
+    const val ONBOARDING_TITLE = "패키와 함께 특별한 선물박스를 만들어보세요."
+
+    // login
+    const val LOGIN_TITLE = "마음으로 채우는 특별한 선물박스"
+
+    // signupNickName
+    const val SIGNUP_NICK_NAME_TITLE = "패키에서 사용할\n닉네임을 입력해주세요"
+    const val SIGNUP_NICK_NAME_DESCRIPTION = "친구들이 나를 잘 알아볼 수 있는 닉네임으로 설정해주세요"
 }

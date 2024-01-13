eb54e95a8f8096f4c64487579a2f33b85085c369
KimHyungSik <colrog968@gmail.com>
2024-01-13 16:36:45 +0900

    NetworkQualifier

---
+++ di/src/main/java/com/packy/di/network/NetworkQualifier.kt
@@ -0,0 +1,11 @@
+package com.packy.di.network
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Youtube
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Packy

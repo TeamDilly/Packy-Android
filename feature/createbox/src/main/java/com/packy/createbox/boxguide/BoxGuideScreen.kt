e0130cd49bfc3bcd841699e93f087daeee2c3a89
KimHyungSik <colrog968@gmail.com>
2024-01-18 00:24:18 +0900

    BottomSheet

--- feature/createbox/src/main/java/com/packy/createbox/boxguide/BoxGuideScreen.kt
+++ feature/createbox/src/main/java/com/packy/createbox/boxguide/BoxGuideScreen.kt
@@ -1,37 +1,21 @@
 package com.packy.createbox.boxguide
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.packy.core.common.Spacer
-import com.packy.core.designsystem.topbar.PackyTopBar
-import com.packy.core.theme.PackyTheme
-import com.packy.core.values.Strings
-import com.packy.core.values.Strings.CRATE_BOX_MUSIC
 import com.packy.createbox.createboax.navigation.CreateBoxNavHost
-import com.packy.di.network.Packy
-import com.packy.feature.core.R
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,18 +23,34 @@ fun BoxGuideScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
 ) {
-    val sheetState = rememberModalBottomSheetState()
-    var showBottomSheet by remember { mutableStateOf(true) }
+    Scaffold { innerPadding ->
+        val sheetState = rememberModalBottomSheetState()
+        var showBottomSheet by remember { mutableStateOf(true) }
+        val scope = rememberCoroutineScope()
 
-    if (showBottomSheet) {
-        ModalBottomSheet(
-            onDismissRequest = {
-                showBottomSheet = false
-            },
-            sheetState = sheetState,
-            dragHandle = null
+        Column(
+            modifier = modifier.padding(innerPadding)
         ) {
-            CreateBoxNavHost { showBottomSheet = false }
+
+        }
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState,
+                dragHandle = null
+            ) {
+                CreateBoxNavHost {
+                    scope.launch { sheetState.hide() }.invokeOnCompletion {
+                        if (!sheetState.isVisible) {
+                            showBottomSheet = false
+                        }
+                    }
+                }
+            }
         }
     }
+
 }

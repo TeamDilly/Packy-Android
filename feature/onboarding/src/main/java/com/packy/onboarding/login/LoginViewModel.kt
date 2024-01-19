0f1ef841421e2eca7638f237c09e44e4e0901af7
KimHyungSik <colrog968@gmail.com>
2024-01-20 02:12:45 +0900

    signInUseCase 사용

--- feature/onboarding/src/main/java/com/packy/onboarding/login/LoginViewModel.kt
+++ feature/onboarding/src/main/java/com/packy/onboarding/login/LoginViewModel.kt
@@ -3,6 +3,7 @@ package com.packy.onboarding.login
 import androidx.lifecycle.viewModelScope
 import com.packy.common.authenticator.KakaoAuth
 import com.packy.common.authenticator.KakaoLoginController
+import com.packy.domain.usecase.auth.SignInUseCase
 import com.packy.domain.usecase.auth.SignUpUseCase
 import com.packy.mvi.base.MviViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,7 +14,8 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val kakaoLoginController: KakaoLoginController,
-    private val signUpUseCase: SignUpUseCase
+    private val signUpUseCase: SignUpUseCase,
+    private val signInUseCase: SignInUseCase
 ) : MviViewModel<LoginIntent, LoginState, LoginEffect>() {
 
     override fun createInitialState(): LoginState = LoginState(
@@ -28,16 +30,29 @@ class LoginViewModel @Inject constructor(
                     is KakaoAuth.KakaoLoginFail -> sendEffect(LoginEffect.KakaoLoginFail)
                     is KakaoAuth.KakaoLoginSuccess -> {
                         viewModelScope.launch {
-                            val signUp = signUpUseCase.getUserSignUpInfo().first()
-                            signUpUseCase.setUserSignUpInfo(signUp.copy(
-                                token = kakaoAuth.token,
-                                provider = "kakao"
-                                ))
-                            sendEffect(LoginEffect.KakaoLoginSuccess)
+                            signInUseCase.signIn(kakaoAuth.token)
+                                .collect{
+
+                                }
                         }
                     }
                 }
             }
         }
     }
+
+    private fun signUp(kakaoAuth: KakaoAuth.KakaoLoginSuccess) {
+        viewModelScope.launch {
+            val signUp = signUpUseCase.getUserSignUpInfo().first()
+            signUpUseCase.setUserSignUpInfo(
+                signUp.copy(
+                    token = kakaoAuth.token,
+                    provider = "kakao"
+                )
+            )
+            sendEffect(LoginEffect.KakaoLoginSuccess)
+        }
+    }
+
+
 }

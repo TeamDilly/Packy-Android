7a13bb17bd5c0fb076f6c68d5c81695fdeb19d7f
KimHyungSik <colrog968@gmail.com>
2024-01-21 02:13:23 +0900

    suggestionMusic suspend

--- data/src/main/java/com/packy/data/remote/music/MusicService.kt
+++ data/src/main/java/com/packy/data/remote/music/MusicService.kt
@@ -7,5 +7,5 @@ import retrofit2.http.GET
 interface MusicService {
 
     @GET("api/v1/admin/music")
-    fun suggestionMusic(): Resource<List<SuggestionMusic>>
+    suspend fun suggestionMusic(): Resource<List<SuggestionMusic>>
 }

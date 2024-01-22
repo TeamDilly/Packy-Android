e9e2374c85b47dc4c039b1b11f0e2d2c72958be4
KimHyungSik <colrog968@gmail.com>
2024-01-23 00:49:51 +0900

    DottedDivider

---
+++ feature/core/src/main/java/com/packy/core/widget/dotted/DottedDivider.kt
@@ -0,0 +1,51 @@
+package com.packy.core.widget.dotted
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.packy.core.theme.PackyTheme
+
+@Composable
+fun DottedDivider(
+    thickness: Dp,
+    color: Color = PackyTheme.color.gray400,
+    phase: Float = 10f,
+    intervals: FloatArray = floatArrayOf(10f, 10f),
+    modifier: Modifier
+){
+    Canvas(
+        modifier = modifier
+    ) {
+        val dividerHeight = thickness.toPx()
+        drawRoundRect(
+            color = color,
+            style = Stroke(
+                width = dividerHeight,
+                pathEffect = PathEffect.dashPathEffect(
+                    intervals = intervals,
+                    phase = phase
+                )
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun DottedDividerPreview() {
+    DottedDivider(
+        color = Color.Black,
+        thickness = 1.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    )
+}

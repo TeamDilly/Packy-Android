3f0f46c5c52dca930abf08451c105bc79d59d624
KimHyungSik <colrog968@gmail.com>
2024-01-23 22:04:11 +0900

    dto parm rename

--- data/src/main/java/com/packy/data/model/BoxDesignDto.kt
+++ data/src/main/java/com/packy/data/model/BoxDesignDto.kt
@@ -7,12 +7,12 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class BoxDesignDto(
     @SerialName("id") val id: Int,
-    @SerialName("boxTopUri") val boxTopUri: String,
-    @SerialName("boxBottomUri") val boxBottomUri: String
+    @SerialName("boxTop") val boxTop: String,
+    @SerialName("boxBottom") val boxBottom: String
 )
 
 fun BoxDesignDto.toEntity(): BoxDesign = BoxDesign(
     id = this.id,
-    boxTopUri = this.boxTopUri,
-    boxBottomUri = this.boxBottomUri
+    boxTopUri = this.boxTop,
+    boxBottomUri = this.boxBottom
 )

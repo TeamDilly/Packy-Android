8fbe01734829e8fcc8f69e259e79bcfa3ce4f1bf
KimHyungSik <colrog968@gmail.com>
2024-02-05 22:12:07 +0900

    home module

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("android.library.feature")
+}
+
+android {
+    namespace = "com.example.home"
+}
+
+dependencies {
+    implementation(project(":di"))
+    implementation(project(":domain"))
+    implementation(project(":library:mvi"))
+    implementation(project(":library:account"))
+    implementation(project(":feature:core"))
+    implementation(project(":common-android"))
+    implementation(project(":lib"))
+}

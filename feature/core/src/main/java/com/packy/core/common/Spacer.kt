7997db635873d97201da1098174b94a8eb16e434
KimHyungSik <colrog968@gmail.com>
2024-02-05 23:26:51 +0900

    HomeGiftBoxes

--- feature/core/src/main/java/com/packy/core/common/Spacer.kt
+++ feature/core/src/main/java/com/packy/core/common/Spacer.kt
@@ -4,18 +4,34 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun RowScope.Spacer(width: Dp) = androidx.compose.foundation.layout.Spacer(modifier = Modifier.width(width))
+fun RowScope.Spacer(width: Dp) =
+    androidx.compose.foundation.layout.Spacer(modifier = Modifier.width(width))
 
 @Composable
-fun RowScope.Spacer(weight: Float) = androidx.compose.foundation.layout.Spacer(modifier = Modifier.weight(weight))
+fun RowScope.Spacer(weight: Float) =
+    androidx.compose.foundation.layout.Spacer(modifier = Modifier.weight(weight))
 
 @Composable
-fun ColumnScope.Spacer(height: Dp) = androidx.compose.foundation.layout.Spacer(modifier = Modifier.height(height))
+fun ColumnScope.Spacer(height: Dp) =
+    androidx.compose.foundation.layout.Spacer(modifier = Modifier.height(height))
 
 @Composable
-fun ColumnScope.Spacer(weight: Float) = androidx.compose.foundation.layout.Spacer(modifier = Modifier.weight(weight))
+fun ColumnScope.Spacer(weight: Float) =
+    androidx.compose.foundation.layout.Spacer(modifier = Modifier.weight(weight))
+
+@Composable
+fun LazyItemScope.Spacer(
+    width: Dp = 0.dp,
+    height: Dp = 0.dp,
+) = androidx.compose.foundation.layout.Spacer(
+    modifier = Modifier
+        .width(width)
+        .height(height)
+)
--- feature/home/src/main/java/com/example/home/home/HomeScreen.kt
+++ feature/home/src/main/java/com/example/home/home/HomeScreen.kt
@@ -3,29 +3,43 @@ package com.example.home.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.packy.feature.core.R
 import com.example.home.navigation.HomeRoute
 import com.packy.core.common.Spacer
 import com.packy.core.common.clickableWithoutRipple
 import com.packy.core.designsystem.topbar.PackyTopBar
 import com.packy.core.theme.PackyTheme
+import com.packy.core.values.Strings
+import com.packy.domain.model.home.HomeBox
 
 @Composable
 fun HomeScreen(
@@ -35,6 +49,9 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+    val giftBoxes = uiState.giftBoxes
+
     LaunchedEffect(viewModel) {
         viewModel.effect.collect { effect ->
             when (effect) {
@@ -93,7 +110,90 @@ fun HomeScreen(
                     ),
             ) {
                 Spacer(height = 24.dp)
+                HomeGiftBoxes(
+                    giftBoxes = giftBoxes
+                )
+                Spacer(height = 24.dp)
             }
         }
+    }
+}
+
+@Composable
+private fun HomeGiftBoxes(giftBoxes: List<HomeBox>) {
+    GiftBoxesTitle(
+        modifier = Modifier
+            .padding(24.dp)
+    )
+    LazyRow(
+        verticalAlignment = Alignment.Top,
+        contentPadding = PaddingValues(horizontal = 16.dp),
+
+        ) {
+        item { Spacer(width = 8.dp) }
+        items(giftBoxes) { homeBox ->
+            GiftBoxItem(
+                modifier = Modifier,
+                homeBox = homeBox
+            )
+        }
+        item { Spacer(width = 8.dp) }
+    }
+}
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+private fun GiftBoxItem(
+    modifier: Modifier = Modifier,
+    homeBox: HomeBox
+) {
+    Column(
+        modifier = modifier
+    ) {
+        GlideImage(
+            modifier = Modifier
+                .height(138.dp)
+                .width(120.dp)
+                .clip(RoundedCornerShape(8.dp)),
+            model = homeBox.boxImageUrl,
+            contentDescription = homeBox.title,
+        )
+        Spacer(height = 12.dp)
+        Text(
+            text = Strings.BOX_ADD_INFO_SENDER + homeBox.sender,
+            style = PackyTheme.typography.body06,
+            color = PackyTheme.color.purple500,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Spacer(height = 4.dp)
+        Text(
+            text = homeBox.title,
+            style = PackyTheme.typography.body05,
+            color = PackyTheme.color.gray900,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@Composable
+private fun GiftBoxesTitle(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier
+    ) {
+        Text(
+            text = Strings.HOME_GIFT_BOXES,
+            style = PackyTheme.typography.heading02,
+            color = PackyTheme.color.gray900,
+        )
+        Spacer(1f)
+        Text(
+            text = Strings.MORE,
+            style = PackyTheme.typography.body03,
+            color = PackyTheme.color.gray900,
+        )
     }
 }

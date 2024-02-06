b6ed9023e8ea79b3fed52e0ba026c71230035585
KimHyungSik <colrog968@gmail.com>
2024-02-07 01:00:28 +0900

    Tab Pager 연결

--- feature/home/src/main/java/com/example/home/mybox/MyBoxScreen.kt
+++ feature/home/src/main/java/com/example/home/mybox/MyBoxScreen.kt
@@ -1,7 +1,11 @@
 package com.example.home.mybox
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
@@ -17,6 +21,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.android.material.tabs.TabLayout
@@ -25,7 +30,11 @@ import com.packy.core.common.clickableWithoutRipple
 import com.packy.core.designsystem.topbar.PackyTopBar
 import com.packy.core.theme.PackyTheme
 import com.packy.feature.core.R
+import com.packy.mvi.ext.emitMviIntent
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MyBoxScreen(
     modifier: Modifier = Modifier,
@@ -37,12 +46,29 @@ fun MyBoxScreen(
     val sendBox = remember { derivedStateOf { uiState.sendBox } }
     val receiveBox = remember { derivedStateOf { uiState.receiveBox } }
 
+    val pagerState = rememberPagerState(pageCount = { 2 })
+
     LaunchedEffect(viewModel) {
         viewModel.effect.collect { effect ->
             when (effect) {
                 is MyBoxEffect.MoveToBack -> navController.popBackStack()
                 is MyBoxEffect.MoveToBoxDetail -> moveToBoxDetail(effect.boxId)
             }
+        }
+    }
+
+    LaunchedEffect(uiState.showTab) {
+        viewModel.uiState
+            .map { it.showTab }
+            .filter { it.ordinal != pagerState.pageCount }
+            .collect {
+                pagerState.animateScrollToPage(it.ordinal)
+            }
+    }
+
+    LaunchedEffect(pagerState) {
+        snapshotFlow { pagerState.currentPage }.collect { page ->
+            viewModel.emitIntent(MyBoxIntent.ChangeShowBoxType(MyBoxType.entries[page]))
         }
     }
 
@@ -58,31 +84,51 @@ fun MyBoxScreen(
         Column(
             modifier = modifier.padding(innerPadding)
         ) {
-            TabRow(
-                selectedTabIndex = uiState.showTab.ordinal,
-                indicator = { tabPositions ->
-                    TabRowDefaults.Indicator(
-                        color = PackyTheme.color.gray900,
-                        modifier = Modifier.tabIndicatorOffset(tabPositions[uiState.showTab.ordinal])
-                    )
-                },
+            MyBoxTab(
+                selectedTab = uiState.showTab,
+                onClick = viewModel::emitIntent
+            )
+            HorizontalPager(
+                modifier = Modifier.fillMaxSize(),
+                state = pagerState
             ) {
-                MyBoxType.entries.forEach { tabType ->
-                    CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
-                        val isSelected = uiState.showTab == tabType
-                        Tab(
-                            text = {
-                                Text(
-                                    text = tabType.title,
-                                    style = if (isSelected) PackyTheme.typography.body01 else PackyTheme.typography.body02,
-                                    color = if (isSelected) PackyTheme.color.gray900 else PackyTheme.color.gray600,
-                                )
-                            },
-                            selected = isSelected,
-                            onClick = { viewModel.emitIntent(MyBoxIntent.ChangeShowBoxType(tabType)) },
-                        )
-                    }
+                when (it) {
+                    0 -> Text(text = "Send")
+                    1 -> Text(text = "Receive")
                 }
+            }
+        }
+    }
+}
+
+@Composable
+private fun MyBoxTab(
+    selectedTab: MyBoxType,
+    onClick: emitMviIntent<MyBoxIntent>
+) {
+    TabRow(
+        selectedTabIndex = selectedTab.ordinal,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                color = PackyTheme.color.gray900,
+                modifier = Modifier.tabIndicatorOffset(tabPositions[selectedTab.ordinal])
+            )
+        },
+    ) {
+        MyBoxType.entries.forEach { tabType ->
+            CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
+                val isSelected = selectedTab == tabType
+                Tab(
+                    text = {
+                        Text(
+                            text = tabType.title,
+                            style = if (isSelected) PackyTheme.typography.body01 else PackyTheme.typography.body02,
+                            color = if (isSelected) PackyTheme.color.gray900 else PackyTheme.color.gray600,
+                        )
+                    },
+                    selected = isSelected,
+                    onClick = { onClick(MyBoxIntent.ChangeShowBoxType(tabType)) },
+                )
             }
         }
     }

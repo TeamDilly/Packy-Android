e386cde2554cc4347c363710e0afe7b460514d90
KimHyungSik <colrog968@gmail.com>
2024-02-13 20:33:13 +0900

    LazyVerticalStaggeredGrid

--- feature/home/src/main/java/com/example/home/archive/ArchiveScreen.kt
+++ feature/home/src/main/java/com/example/home/archive/ArchiveScreen.kt
@@ -1,12 +1,25 @@
 package com.example.home.archive
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.packy.core.common.clickableWithoutRipple
+import com.packy.core.theme.PackyTheme
 
 @Composable
 fun ArchiveScreen(
@@ -15,8 +28,29 @@ fun ArchiveScreen(
     viewModel: ArchiveViewModel = hiltViewModel()
 ) {
     Scaffold { innerPadding ->
-        Box(modifier = modifier.padding(innerPadding)) {
-
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            LazyVerticalStaggeredGrid(
+                modifier = Modifier.fillMaxSize(),
+                columns = StaggeredGridCells.Fixed(2),
+                verticalItemSpacing = 4.dp,
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+                item {
+                    Spacer(modifier = Modifier.height(80.dp))
+                }
+                items(100) {
+                    Box(
+                        modifier = Modifier
+                            .size(200.dp)
+                            .fillMaxSize()
+                            .background(color = PackyTheme.color.purple500)
+                    )
+                }
+            }
         }
     }
 }

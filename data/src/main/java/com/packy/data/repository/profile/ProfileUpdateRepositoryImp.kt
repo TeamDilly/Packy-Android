718d607fdb10ea4080982dec35c26e5885835b0b
KimHyungSik <colrog968@gmail.com>
2024-02-20 23:23:26 +0900

    updateNickNameUseCase 연결

--- data/src/main/java/com/packy/data/repository/profile/ProfileUpdateRepositoryImp.kt
+++ data/src/main/java/com/packy/data/repository/profile/ProfileUpdateRepositoryImp.kt
@@ -1,5 +1,6 @@
 package com.packy.data.repository.profile
 
+import com.packy.account.AccountManagerHelper
 import com.packy.data.remote.profile.ProfileUpdateService
 import com.packy.domain.repository.profile.ProfileUpdateRepository
 import com.packy.lib.utils.Resource
@@ -9,7 +10,8 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ProfileUpdateRepositoryImp @Inject constructor(
-    private val profileUpdateService: ProfileUpdateService
+    private val profileUpdateService: ProfileUpdateService,
+    private val accountManagerHelper: AccountManagerHelper
 ) : ProfileUpdateRepository {
     override suspend fun updateNickName(nickname: String): Flow<Resource<Unit>> = flow {
         emit(Resource.Loading())
@@ -20,6 +22,9 @@ class ProfileUpdateRepositoryImp @Inject constructor(
     override suspend fun updateProfile(profileId: Int): Flow<Resource<Unit>> = flow {
         emit(Resource.Loading())
         val updateResource = profileUpdateService.updateProfile(profileImg =  profileId)
+        if(updateResource is Resource.Success){
+            accountManagerHelper.setNickName(updateResource.data.nickname)
+        }
         emit(updateResource.map {  })
     }
 }
--- feature/home/src/main/java/com/example/home/nickname/SettingNicknameViewModel.kt
+++ feature/home/src/main/java/com/example/home/nickname/SettingNicknameViewModel.kt
@@ -3,6 +3,9 @@ package com.example.home.nickname
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.example.home.navigation.SettingsArgs
+import com.packy.domain.usecase.profile.UpdateNickNameUseCase
+import com.packy.lib.utils.filterSuccess
+import com.packy.lib.utils.unwrapResource
 import com.packy.mvi.base.MviViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -10,6 +13,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SettingNicknameViewModel @Inject constructor(
+    private val updateNickNameUseCase: UpdateNickNameUseCase,
     private val savedStateHandle: SavedStateHandle
 ) :
     MviViewModel<SettingNicknameIntent, SettingNicknameState, SettingNicknameEffect>() {
@@ -20,7 +24,21 @@ class SettingNicknameViewModel @Inject constructor(
     override fun handleIntent() {
         subscribeIntent<SettingNicknameIntent.OnBackClick> { sendEffect(SettingNicknameEffect.MoveToBack) }
         subscribeIntent<SettingNicknameIntent.OnSettingProfileClick> { sendEffect(SettingNicknameEffect.MoveToSettingProfile(currentState.profileImage)) }
-        subscribeIntent<SettingNicknameIntent.OnSaveClick> {  }
+        subscribeIntent<SettingNicknameIntent.OnSaveClick> {
+            currentState.currentNickName?.let { nickname ->
+                updateNickNameUseCase.updateNickName(nickname)
+                    .filterSuccess()
+                    .unwrapResource()
+                    .collect{
+                        setState {
+                            it.copy(
+                                prevNickname = currentState.currentNickName
+                            )
+                        }
+                        sendEffect(SettingNicknameEffect.MoveToBack)
+                    }
+            }
+        }
         subscribeStateIntent<SettingNicknameIntent.ChangeNickName>{ state, intent ->
             state.copy(
                 currentNickName = intent.nickname

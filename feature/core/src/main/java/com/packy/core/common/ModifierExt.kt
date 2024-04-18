2ccd776898bae42809db4722d562130f823a66a3
KimHyungSik <colrog968@gmail.com>
2024-04-18 22:30:07 +0900

    conditionalBorder

--- feature/core/src/main/java/com/packy/core/common/ModifierExt.kt
+++ feature/core/src/main/java/com/packy/core/common/ModifierExt.kt
@@ -1,12 +1,19 @@
 package com.packy.core.common
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.packy.core.theme.PackyTheme
 
 @SuppressLint("ModifierFactoryUnreferencedReceiver")
 fun Modifier.clickableWithoutRipple(
@@ -27,3 +34,21 @@ fun Modifier.clickableWithoutRipple(
         onClick = onClick
     )
 }
+
+@Composable
+fun Modifier.conditionalBorder(
+    condition: Boolean,
+    width: Dp = 1.dp,
+    shape: Shape = RectangleShape,
+    color: @Composable () -> Color = { PackyTheme.color.black }
+): Modifier = Modifier.then(
+    if (condition) {
+        this.border(
+            width = width,
+            color = color(),
+            shape = shape
+        )
+    } else {
+        Modifier
+    }
+)

ecc827b8713e85d3c42eeb387a97b3c9653767e6
KimHyungSik <colrog968@gmail.com>
2024-09-07 15:34:59 +0900

    banner text

--- feature/core/src/main/java/com/packy/core/widget/banner/SmallScrollBanner.kt
+++ feature/core/src/main/java/com/packy/core/widget/banner/SmallScrollBanner.kt
@@ -1,21 +1,24 @@
 package com.packy.core.widget.banner
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.packy.core.common.clickableWithoutRipple
+import com.packy.core.theme.PackyTheme
 import com.packy.domain.model.banner.ImageBanner
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -67,6 +70,24 @@ fun SmallScrollBanner(
                 model = item.imageUrl,
                 contentDescription = "banner Image",
                 contentScale = ContentScale.Crop
+            )
+        }
+        if (bannerUrlList.size > 1) {
+            Text(
+                modifier = Modifier
+                    .padding(
+                        end = 12.dp,
+                        bottom = 12.dp
+                    )
+                    .background(
+                        color = PackyTheme.color.black.copy(alpha = 0.6f),
+                        shape = RoundedCornerShape(20.dp)
+                    )
+                    .align(Alignment.BottomEnd)
+                    .padding(horizontal = 8.dp, vertical = 2.dp),
+                text = "${(pagerState.currentPage % bannerUrlList.size) + 1} / ${bannerUrlList.size}",
+                style = PackyTheme.typography.body06,
+                color = PackyTheme.color.white
             )
         }
     }
